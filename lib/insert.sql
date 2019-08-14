c146a42060be26bacdf6f8490992ab4301b64cd3
Audrea Cook <audreacwork@gmail.com>
2019-08-14 16:48:39 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,124 @@
+INSERT INTO users (name, age)
+VALUES ("Aud", 30);
+INSERT INTO users (name, age)
+VALUES ("Dex", 30);
+INSERT INTO users (name, age)
+VALUES ("Adam", 29);
+INSERT INTO users (name, age)
+VALUES ("Derek", 28);
+INSERT INTO users (name, age)
+VALUES ("Alyssa", 27);
+INSERT INTO users (name, age)
+VALUES ("Kari", 26);
+INSERT INTO users (name, age)
+VALUES ("Kourtney", 32);
+INSERT INTO users (name, age)
+VALUES ("Justin", 32);
+INSERT INTO users (name, age)
+VALUES ("Bennett", 8);
+INSERT INTO users (name, age)
+VALUES ("Haylie", 6);
+INSERT INTO users (name, age)
+VALUES ("Layton", 4);
+INSERT INTO users (name, age)
+VALUES ("Orion", 1);
+INSERT INTO users (name, age)
+VALUES ("Brandon", 26);
+INSERT INTO users (name, age)
+VALUES ("Kyal", 28);
+INSERT INTO users (name, age)
+VALUES ("Crystal", 40);
+INSERT INTO users (name, age)
+VALUES ("Ezra", 22);
+INSERT INTO users (name, age)
+VALUES ("Ivy", 18);
+INSERT INTO users (name, age)
+VALUES ("Gavin", 16);
+INSERT INTO users (name, age)
+VALUES ("Patricia", 54);
+INSERT INTO users (name, age)
+VALUES ("Troy", 54);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Hera', 'goddess', 1520, 04/05/2019, 12/31/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Athena', 'goddess', 150030, 02/25/2019, 11/31/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Persephone', 'goddess', 20000, 07/16/2019, 03/01/2020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Hecate', 'goddess', 250350, 11/04/2018, 10/15/2021);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Zeus', 'god', 1500, 07/20/2019, 12/18/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Poseidon', 'god', 19500, 02/03/2019, 09/25/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Hades', 'god', 152360, 06/25/2019, 10/01/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Achilles', 'demigod', 505050, 07/15/2019, 11/31/2020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Peleus', 'demigod', 1800, 07/15/2019, 08/15/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Aeacus', 'demigod', 120530, 06/16/2019, 04/18/2020);
+
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 15, 1);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (200, 11, 5);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (100, 1, 2);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (580, 6, 7);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 20, 6);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 6, 10);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 16, 1);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 19, 2);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 1, 7);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 2, 9);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 18, 10);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 7, 3);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 5, 4);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 16, 5);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 17, 2);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 15, 9);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 11, 10);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 7, 7);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 12, 4);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 13, 6);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 17, 10);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 8, 2);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 12, 3);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 19, 9);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 16, 1);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 7, 5);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 13, 9);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 13, 1);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 15, 4);
+INSERT INTO pledges(amount, user_id, project_id)
+VALUES (500, 17, 7);
+
+
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,65 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  # I need to show project.title and the pledge amounts from Projects
+  # I need to sum the pledge.amount and group by project_id
+  "SELECT projects.title, SUM(pledges.amount) AS pledge_amount FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY pledges.project_id
+ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Write your SQL query Here"
+  # I need the user.name, user.age, and the pledge amount from Users
+  # I need to sum the pledge.amount and group by user_id
+  # I need to order by user.name
+  "SELECT users.name, users.age, SUM(pledges.amount) AS pledged_amount
+FROM users
+JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY pledges.user_id
+ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  # I need the projects.title and SUM(pledges.amount - projects.funding_goal)
+  # where projects.funding_goal <= pledges.amount from Projects
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS extra
+FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY pledges.project_id
+HAVING extra >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  # I need the user.name and SUM(pledges.amount) as summed_amount FROM users
+  # grouped by user.name
+  # order by summed_amount
+  "SELECT users.name, SUM(pledges.amount) AS summed_amount
+FROM users
+JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY summed_amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  # I need the projects.category and the sum of the category pledge from projects
+  "SELECT projects.category, pledges.amount AS pledged_amount
+FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  # I need the project.category and SUM(pledges.amount) from projects
+  # where projects.category = 'books'
+  "SELECT projects.category, SUM(pledges.amount) AS pledged_amount
+FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'books';"
 end
